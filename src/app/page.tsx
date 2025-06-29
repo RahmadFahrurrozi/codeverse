4a11d101fdbbe456ea5d7e5bd961144a8ae6c79e
RahmaFahrurrozi <zfahrur150105@gmail.com>
2025-06-29 11:58:57 +0700

    feat: implement responsive navigation with Desktop and Mobile navbars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,7 @@
-import { ModeToggle } from "@/components/mode-toogle";
-import { Button } from "@/components/ui/button";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
-
 export default function Home() {
   return (
-    <header className="flex justify-end items-center p-4 gap-2 h-16">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button className="cursor-pointer">Sign In</Button>
-        </SignInButton>
-        <SignUpButton>
-          <Button className="cursor-pointer" variant={"outline"}>
-            Sign Up
-          </Button>
-        </SignUpButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle />
-    </header>
+    <div>
+      <h1>Home page content</h1>
+    </div>
   );
 }
